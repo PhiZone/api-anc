7767328cf28a643e6ac4b0724275e539430bfd72
purofle <purofle@gmail.com>
2023-06-17 23:37:57 +0800

    refactor: use PostgreSQL

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddIdentity<User, Role>()
