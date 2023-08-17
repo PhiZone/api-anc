4be317ed25ec827f286bbf80b6ee068d5a9b233b
Naptie <naptie@qq.com>
2023-08-18 00:51:36 +0800

    refactor: song edition type

--- Enums/EditionType.cs
+++ Enums/EditionType.cs
@@ -4,7 +4,7 @@ public enum EditionType
 {
     Original, // the original edition of the song
     EditedByAuthor, // an edition of the song made by the author themselves
-    EditedByFirstParty, // a version edited by a first party (licensed by the author)
+    EditedBySecondParty, // a version edited by a second party (licensed by the author)
     EditedByUploaderLicensed, // a version edited by the uploader (licensed by the author)
     EditedByUploaderUnlicensed, // a version edited by the uploader (not licensed by the author)
     EditedByThirdParty // a version edited by a third party (not licensed by the author)
