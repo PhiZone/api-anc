2cc0bff6fa789235cd83804cd64557a9a2825b76
Naptie <naptie@qq.com>
2023-08-28 00:45:54 +0800

    refactor: use kawazu

--- Services/FileStorageService.cs
+++ Services/FileStorageService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web;
+using Kawazu;
 using LeanCloud;
 using LeanCloud.Storage;
 using Microsoft.Extensions.Options;
@@ -25,7 +26,7 @@ public class FileStorageService : IFileStorageService
         await formFile.CopyToAsync(memoryStream);
         var extension = FileTypeResolver.GetFileExtension(FileTypeResolver.GetMimeType(formFile));
         var file = new LCFile(
-            $"{typeof(T).Name}_{NormalizeFileName(fileName)}_{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}{extension}",
+            $"{typeof(T).Name}_{await NormalizeFileName(fileName)}_{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}{extension}",
             memoryStream.ToArray());
         await file.Save();
         return (file.Url, (string)file.MetaData["_checksum"]);
@@ -46,23 +47,23 @@ public class FileStorageService : IFileStorageService
     public async Task<(string, string)> Upload<T>(string fileName, MemoryStream stream, string extension)
     {
         var file = new LCFile(
-            $"{typeof(T).Name}_{NormalizeFileName(fileName)}_{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}.{extension}",
+            $"{typeof(T).Name}_{await NormalizeFileName(fileName)}_{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}.{extension}",
             stream.ToArray());
         await file.Save();
         return (file.Url, (string)file.MetaData["_checksum"]);
     }
 
-    private static string NormalizeFileName(string input)
+    private static async Task<string> NormalizeFileName(string input)
     {
         var chars = Path.GetInvalidFileNameChars().Concat(new[] { ' ' });
-        return HttpUtility.UrlEncode(chars.Aggregate(Romanize(input),
+        return HttpUtility.UrlEncode(chars.Aggregate(await Romanize(input),
             (current, invalidChar) => current.Replace(invalidChar.ToString(), string.Empty)));
     }
 
-    private static string Romanize(string input)
+    private static async Task<string> Romanize(string input)
     {
-        var japanese = new Japanese.KanjiReadings();
-        input = japanese.Process(input);
+        var japanese = new KawazuConverter();
+        input = await japanese.Convert(input, To.Romaji);
         var korean = new Korean.RevisedRomanization();
         input = korean.Process(input);
         var russian = new Russian.BgnPcgn();
