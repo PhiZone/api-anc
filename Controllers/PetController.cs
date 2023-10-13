09fa79897a5ae7997ca256c73bfdabb940fefc88
Naptie <naptie@qq.com>
2023-10-13 19:29:33 +0800

    feat: pet answer retrieval

--- Controllers/PetController.cs
+++ Controllers/PetController.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using OpenIddict.Abstractions;
 using OpenIddict.Validation.AspNetCore;
+using PhiZoneApi.Configurations;
 using PhiZoneApi.Constants;
 using PhiZoneApi.Dtos.Deliverers;
+using PhiZoneApi.Dtos.Filters;
 using PhiZoneApi.Dtos.Requests;
 using PhiZoneApi.Dtos.Responses;
 using PhiZoneApi.Enums;
+using PhiZoneApi.Filters;
 using PhiZoneApi.Interfaces;
 using PhiZoneApi.Models;
 using StackExchange.Redis;
@@ -26,17 +30,23 @@ public class PetController : Controller
     private readonly IPetQuestionRepository _petQuestionRepository;
     private readonly IConnectionMultiplexer _redis;
     private readonly IResourceService _resourceService;
+    private readonly IOptions<DataSettings> _dataSettings;
+    private readonly IDtoMapper _dtoMapper;
+    private readonly IFilterService _filterService;
     private readonly Dictionary<Role, int> _scores;
     private readonly UserManager<User> _userManager;
 
     public PetController(IConnectionMultiplexer redis, IPetQuestionRepository petQuestionRepository,
         IPetAnswerRepository petAnswerRepository, UserManager<User> userManager, IResourceService resourceService,
-        IConfiguration config)
+        IConfiguration config, IOptions<DataSettings> dataSettings, IDtoMapper dtoMapper, IFilterService filterService)
     {
         _redis = redis;
         _petQuestionRepository = petQuestionRepository;
         _userManager = userManager;
         _resourceService = resourceService;
+        _dataSettings = dataSettings;
+        _dtoMapper = dtoMapper;
+        _filterService = filterService;
         _petAnswerRepository = petAnswerRepository;
         _scores = new Dictionary<Role, int>
             {
@@ -266,6 +276,84 @@ public class PetController : Controller
     }
 
     /// <summary>
+    ///     Retrieves answers.
+    /// </summary>
+    /// <returns>An array of answers.</returns>
+    /// <response code="200">Returns an array of answers.</response>
+    /// <response code="400">When any of the parameters is invalid.</response>
+    [HttpGet("answers")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseDto<IEnumerable<PetAnswerDto>>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseDto<object>))]
+    public async Task<IActionResult> GetAnswers([FromQuery] ArrayRequestDto dto,
+        [FromQuery] PetAnswerFilterDto? filterDto = null)
+    {
+        var currentUser = (await _userManager.FindByIdAsync(User.GetClaim(OpenIddictConstants.Claims.Subject)!))!;
+        var isVolunteer = await _resourceService.HasPermission(currentUser, Roles.Volunteer);
+        dto.PerPage = dto.PerPage > 0 && dto.PerPage < _dataSettings.Value.PaginationMaxPerPage ? dto.PerPage :
+            dto.PerPage == 0 ? _dataSettings.Value.PaginationPerPage : _dataSettings.Value.PaginationMaxPerPage;
+        dto.Page = dto.Page > 1 ? dto.Page : 1;
+        var position = dto.PerPage * (dto.Page - 1);
+        var predicateExpr = await _filterService.Parse(filterDto, dto.Predicate, currentUser,
+            e => isVolunteer || e.OwnerId == currentUser.Id);
+        var answers = await _petAnswerRepository.GetPetAnswersAsync(dto.Order, dto.Desc, position, dto.PerPage,
+            dto.Search, predicateExpr);
+        var total = await _petAnswerRepository.CountPetAnswersAsync(dto.Search, predicateExpr);
+        var list = new List<PetAnswerDto>();
+
+        foreach (var answer in answers) list.Add(await _dtoMapper.MapPetAnswerAsync<PetAnswerDto>(answer));
+
+        return Ok(new ResponseDto<IEnumerable<PetAnswerDto>>
+        {
+            Status = ResponseStatus.Ok,
+            Code = ResponseCodes.Ok,
+            Total = total,
+            PerPage = dto.PerPage,
+            HasPrevious = position > 0,
+            HasNext = dto.PerPage > 0 && dto.PerPage * dto.Page < total,
+            Data = list
+        });
+    }
+
+    /// <summary>
+    ///     Retrieves a specific answer.
+    /// </summary>
+    /// <param name="id">An answer's ID.</param>
+    /// <returns>A answer.</returns>
+    /// <response code="200">Returns an answer.</response>
+    /// <response code="304">
+    ///     When the resource has not been updated since last retrieval. Requires <c>If-None-Match</c>.
+    /// </response>
+    /// <response code="400">When any of the parameters is invalid.</response>
+    /// <response code="404">When the specified answer is not found.</response>
+    [HttpGet("answers/{id:guid}")]
+    [ServiceFilter(typeof(ETagFilter))]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseDto<PetAnswerDto>))]
+    [ProducesResponseType(typeof(void), StatusCodes.Status304NotModified, "text/plain")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseDto<object>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseDto<object>))]
+    public async Task<IActionResult> GetAnswer([FromRoute] Guid id)
+    {
+        var currentUser = (await _userManager.FindByIdAsync(User.GetClaim(OpenIddictConstants.Claims.Subject)!))!;
+        if (!await _petAnswerRepository.PetAnswerExistsAsync(id))
+            return NotFound(new ResponseDto<object>
+            {
+                Status = ResponseStatus.ErrorBrief, Code = ResponseCodes.ResourceNotFound
+            });
+        var answer = await _petAnswerRepository.GetPetAnswerAsync(id);
+        if (answer.OwnerId != currentUser.Id && !await _resourceService.HasPermission(currentUser, Roles.Volunteer))
+            return StatusCode(StatusCodes.Status403Forbidden,
+                new ResponseDto<object>
+                {
+                    Status = ResponseStatus.ErrorBrief, Code = ResponseCodes.InsufficientPermission
+                });
+        var dto = await _dtoMapper.MapPetAnswerAsync<PetAnswerDto>(answer);
+
+        return Ok(new ResponseDto<PetAnswerDto> { Status = ResponseStatus.Ok, Code = ResponseCodes.Ok, Data = dto });
+    }
+
+    /// <summary>
     ///     Reviews an answer.
     /// </summary>
     /// <param name="id">An answer's ID.</param>
@@ -276,8 +364,7 @@ public class PetController : Controller
     /// <response code="403">When the user does not have sufficient permission.</response>
     /// <response code="404">When the specified pet answer is not found.</response>
     /// <response code="500">When an internal server error has occurred.</response>
-    [HttpPatch("{id:guid}")]
-    [Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
+    [HttpPatch("answers/{id:guid}")]
     [Consumes("application/json")]
     [Produces("application/json")]
     [ProducesResponseType(typeof(void), StatusCodes.Status204NoContent, "text/plain")]
---
+++ Dtos/Responses/PetAnswerDto.cs
@@ -0,0 +1,34 @@
+﻿namespace PhiZoneApi.Dtos.Responses;
+
+public class PetAnswerDto
+{
+    public Guid Id { get; set; }
+
+    public PetQuestionDto? Question1 { get; set; }
+
+    public string Answer1 { get; set; } = null!;
+
+    public PetQuestionDto? Question2 { get; set; }
+
+    public string Answer2 { get; set; } = null!;
+
+    public PetQuestionDto? Question3 { get; set; }
+
+    public string Answer3 { get; set; } = null!;
+
+    public string Chart { get; set; } = null!;
+
+    public int ObjectiveScore { get; set; }
+
+    public int? SubjectiveScore { get; set; }
+
+    public int? TotalScore { get; set; }
+
+    public int OwnerId { get; set; }
+
+    public int? AssessorId { get; set; }
+
+    public DateTimeOffset DateCreated { get; set; }
+
+    public DateTimeOffset DateUpdated { get; set; }
+}
