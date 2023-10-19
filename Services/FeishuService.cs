07a58bdd1b640a4af5ddf8f8ac105b5b1933d248
Naptie <naptie@qq.com>
2023-10-19 21:03:02 +0800

    feat: feishu notification on new pet answers

--- Services/FeishuService.cs
+++ Services/FeishuService.cs
@@ -139,6 +139,52 @@ public class FeishuService : IFeishuService
         }
     }
 
+    public async Task Notify(PetAnswer answer, DateTimeOffset dateStarted, params int[] chats)
+    {
+        await UpdateToken();
+        await using var scope = _provider.CreateAsyncScope();
+        var userManager = scope.ServiceProvider.GetService<UserManager<User>>()!;
+
+        var duration = DateTimeOffset.UtcNow - dateStarted;
+
+        var variables = new Dictionary<string, string>
+        {
+            { "answerer", (await userManager.FindByIdAsync(answer.OwnerId.ToString()))!.UserName! },
+            { "objective_score", answer.ObjectiveScore.ToString() },
+            { "time", dateStarted.ToString("yyyy-MM-dd HH:mm") },
+            { "duration", duration.ToString("F0") },
+            { "answer_info", $"{_config["WebsiteURL"]}/pet/answers/{answer.Id}" }
+        };
+        var content =
+            $"{{\"type\":\"template\",\"data\":{{\"template_id\":\"{_feishuSettings.Value.Cards[FeishuResources.ChartCard]}\",\"template_variable\":{JsonConvert.SerializeObject(variables)}}}}}";
+        foreach (var chat in chats)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri =
+                    new Uri($"{_feishuSettings.Value.ApiUrl}/open-apis/im/v1/messages?receive_id_type=chat_id"),
+                Headers = { { "Authorization", $"Bearer {_token}" } },
+                Content = new StringContent(JsonConvert.SerializeObject(new FeishuMessageDto
+                {
+                    ReceiveId = _feishuSettings.Value.Chats[chat],
+                    MessageType = "interactive",
+                    Content = content
+                }), Encoding.UTF8, "application/json")
+            };
+            _logger.LogInformation(JsonConvert.SerializeObject(new FeishuMessageDto
+            {
+                ReceiveId = _feishuSettings.Value.Chats[chat],
+                MessageType = "interactive",
+                Content = content
+            }));
+            var response = await _client.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+                _logger.LogError(LogEvents.FeishuFailure, "An error occurred whilst announcing PET answer update:\n{Error}",
+                    await response.Content.ReadAsStringAsync());
+        }
+    }
+
     private async Task UpdateToken()
     {
         var now = DateTimeOffset.UtcNow;
@@ -148,7 +194,6 @@ public class FeishuService : IFeishuService
         {
             Method = HttpMethod.Post,
             RequestUri = new Uri($"{_feishuSettings.Value.ApiUrl}/open-apis/auth/v3/tenant_access_token/internal"),
-            // Headers = { { "Content-Type", "application/json; charset=utf-8" } },
             Content = new StringContent(
                 JsonConvert.SerializeObject(new FeishuTokenDto
                 {
