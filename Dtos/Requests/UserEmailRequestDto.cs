208f63065a418da60e9cc4b89d8934ecf746aef0
Naptie <naptie@qq.com>
2023-11-05 11:55:50 +0800

    fix: email confirmation field validation

--- Dtos/Requests/UserEmailRequestDto.cs
+++ Dtos/Requests/UserEmailRequestDto.cs
@@ -8,12 +8,15 @@ namespace PhiZoneApi.Dtos.Requests;
 public class UserEmailRequestDto
 {
     [Required(ErrorMessage = ResponseCodes.FieldEmpty)]
+    [EmailAddress(ErrorMessage = ResponseCodes.InvalidEmailAddress)]
     [MaxLength(1000, ErrorMessage = ResponseCodes.ValueTooLong)]
     [UserInputValidator(ErrorMessage = ResponseCodes.ContentProhibited)]
     public string Email { get; set; } = null!;
 
     [Required(ErrorMessage = ResponseCodes.FieldEmpty)]
-    [MaxLength(1000, ErrorMessage = ResponseCodes.ValueTooLong)]
+    [RegularExpression(
+        @"^([a-zA-Z0-9_\u4e00-\u9fff\u3041-\u309f\u30a0-\u30ff\uac00-\ud7a3]{3,12})|([\u4e00-\u9fff\u3041-\u309f\u30a0-\u30ff\uac00-\ud7a3]{2,12})|([A-Za-z0-9_]{4,18})$",
+        ErrorMessage = ResponseCodes.InvalidUserName)]
     [UserInputValidator(ErrorMessage = ResponseCodes.ContentProhibited)]
     public string UserName { get; set; } = null!;
 
