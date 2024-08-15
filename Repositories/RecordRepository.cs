bb83c88eb993f2d5ce8ff78103fff8b1afd725ec
Naptie <naptie@qq.com>
2024-08-15 03:36:54 -0700

    fix: account inheritance

--- Repositories/RecordRepository.cs
+++ Repositories/RecordRepository.cs
@@ -82,14 +82,15 @@ public class RecordRepository(ApplicationDbContext context, IMeilisearchService 
 
     public async Task<bool> CreateRecordAsync(Record record)
     {
-        var chart = await context.Charts.Include(e => e.Song)
-            .ThenInclude(e => e.Tags)
-            .FirstAsync(e => e.Id == record.ChartId);
-        chart.PlayCount = await context.Records.LongCountAsync(e => e.ChartId == record.Chart.Id) + 1;
-        chart.Song.PlayCount = await context.Records.LongCountAsync(e => e.Chart.SongId == record.Chart.SongId) + 1;
+        var chart = await context.Charts.Include(e => e.Song).FirstAsync(e => e.Id == record.ChartId);
+        chart.PlayCount = await context.Records.LongCountAsync(e => e.ChartId == record.ChartId) + 1;
+        var song = await context.Songs.FirstAsync(e => e.Id == chart.SongId);
+        song.PlayCount = await context.Records.LongCountAsync(e => e.Chart.SongId == chart.SongId) + 1;
         await context.Records.AddAsync(record);
-        await meilisearchService.UpdateAsync(record.Chart);
-        await meilisearchService.UpdateAsync(record.Chart.Song);
+        context.Charts.Update(chart);
+        context.Songs.Update(song);
+        await meilisearchService.UpdateAsync(chart);
+        await meilisearchService.UpdateAsync(song);
         return await SaveAsync();
     }
 
