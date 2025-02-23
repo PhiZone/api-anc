269e4afb3aaf203865461384fe830ed7fe1568ac
Naptie <naptie@qq.com>
2025-02-23 16:30:06 +0800

    refactor: update SeekTune service interface

--- Interfaces/ISeekTuneService.cs
+++ Interfaces/ISeekTuneService.cs
@@ -7,9 +7,9 @@ public interface ISeekTuneService
 {
     Task InitializeAsync(ApplicationDbContext context, CancellationToken cancellationToken);
 
-    Task<List<SeekTuneFindResult>?> FindMatches(string pathToSong, bool resourceRecords = false);
+    Task<List<SeekTuneFindResult>?> FindMatches(string pathToSong, bool resourceRecords = false, int take = -1);
 
-    Task<bool> CreateFingerprint(Guid id, string title, string artist, string songLocation, bool isUrl = false,
+    Task<bool> CreateFingerprint(Guid id, string title, string? version, string artist, string songLocation, bool isUrl = false,
         bool resourceRecords = false);
 
     Task<bool> CheckIfExists(Guid id, bool resourceRecords = false);
