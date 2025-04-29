5b684a01ea0af665ea836663b96f65c20279bcaa
Naptie <naptie@qq.com>
2025-04-29 09:06:26 +0800

    refactor(SubmissionHub): _userGroupDictionary

--- Hubs/SubmissionHub.cs
+++ Hubs/SubmissionHub.cs
@@ -11,16 +11,21 @@ public class SubmissionHub(IResourceService resourceService, ILogger<SubmissionH
     public async Task Register(Guid sessionId)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, sessionId.ToString());
-        _userGroupDictionary[Context.ConnectionId] = sessionId;
+        _userGroupDictionary.Add(Context.ConnectionId, sessionId);
         logger.LogInformation(LogEvents.SubmissionHubInfo, "Registered user {ConnectionId} with session {SessionId}",
             Context.ConnectionId, sessionId);
     }
 
     public override async Task<Task> OnDisconnectedAsync(Exception? exception)
     {
-        await Groups.RemoveFromGroupAsync(Context.ConnectionId, _userGroupDictionary[Context.ConnectionId].ToString());
-        await resourceService.CleanupSession(_userGroupDictionary[Context.ConnectionId]);
-        _userGroupDictionary.Remove(Context.ConnectionId);
+        // ReSharper disable once InvertIf
+        if (_userGroupDictionary.TryGetValue(Context.ConnectionId, out var sessionId))
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, sessionId.ToString());
+            await resourceService.CleanupSession(sessionId);
+            _userGroupDictionary.Remove(Context.ConnectionId);
+        }
+
         return base.OnDisconnectedAsync(exception);
     }
 }
