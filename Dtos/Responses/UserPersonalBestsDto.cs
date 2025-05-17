93f47e44c06107091b70aa97c977f8b2cf467fa5
Naptie <naptie@qq.com>
2025-05-18 02:05:04 +0800

    fix(UserPersonalBestsDto): Non-nullable property 'Phi3' must contain a non-null value when exiting constructor.

--- Dtos/Responses/UserPersonalBestsDto.cs
+++ Dtos/Responses/UserPersonalBestsDto.cs
@@ -2,7 +2,7 @@
 
 public class UserPersonalBestsDto
 {
-    public List<RecordDto> Phi3 { get; set; }
+    public List<RecordDto> Phi3 { get; set; } = null!;
 
     public List<RecordDto> Best27 { get; set; } = null!;
 }
